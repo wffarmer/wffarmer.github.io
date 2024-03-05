ba50dc292889724154b722d4a1426ef9415576c4
wffarmer <159940261+wffarmer@users.noreply.github.com>
2024-03-05 11:16:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 const hamburger = document.getElementById("burgerbars");
 const leftmenu = document.querySelector(".leftmenu");
 
-
 hamburger.addEventListener("click", () => {
     const leftmenu = document.querySelector(".leftmenu");
     leftmenu.classList.toggle("active");
 })
 
+const theme = document.querySelector(".theme");
+const themeSelect = document.querySelector("select[name='theme']");
+
+themeSelect.addEventListener("change", function() {
+    const selectedTheme = this.value;
+  
+    const burgerImg = document.getElementById("burgerbars");
+    const logoImg = document.getElementById("logo").querySelector("img");
+
+    if (selectedTheme === "darktheme") {
+        burgerImg.src = "images/burger.png"; 
+        logoImg.src = "images/logo.png";
+        theme.classList.add("darktheme");
+        theme.classList.remove("lighttheme"); 
+        document.querySelector(".leftmenu.active").style.backgroundColor = "var(--darkbgmenu)";
+    } else if (selectedTheme === "lighttheme") {
+        burgerImg.src = "images/lightburger.png";
+        logoImg.src = "images/lightlogo.png";
+        theme.classList.add("lighttheme"); 
+        theme.classList.remove("darktheme"); 
+        document.querySelector(".leftmenu.active").style.backgroundColor = "var(--lightbgmenu)";
+    }
+  });
 
 
 
