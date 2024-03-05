51025217f84dfca12c950c9ed8e48e5ff41d138f
wffarmer <159940261+wffarmer@users.noreply.github.com>
2024-03-05 09:22:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const hamburger = document.getElementById("burgerbars");
+
+hamburger.addEventListener("click", () => {
+    const leftmenu = document.querySelector(".leftmenu");
+    leftmenu.classList.toggle("active");
+})
+
+
+
+
 
